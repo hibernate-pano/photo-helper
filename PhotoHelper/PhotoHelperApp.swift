8e43e47fcba58a54ae3d17b792c8b7518b8b8dd3
panbo <panbo.coding@qq.com>
2024-09-13 15:19:19 +0800

    Initial Commit

---
+++ PhotoHelper/PhotoHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoHelperApp.swift
+//  PhotoHelper
+//
+//  Created by panbo on 2024/9/13.
+//
+
+import SwiftUI
+
+@main
+struct PhotoHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
